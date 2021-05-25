84c36c74fd4b41732ebe9d827b831512e2427d42
SijyKijy <SijyKijy@users.noreply.github.com>
2021-05-25 13:52:29 +0300

    Add first logic

--- WebServerExample/Program.cs
+++ WebServerExample/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-namespace WebServerExample
+namespace Server
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static readonly int _port = 50501;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endpoint = new(IPAddress.Any, _port);
+
+            socket.Bind(endpoint);
+            socket.Listen(4);
+
+            var responseSocket = socket.Accept();
+
+            Span<byte> buffer = new(new byte[1024]);
+            StringBuilder sb = new(responseSocket.Available);
+
+            do
+            {
+                responseSocket.Receive(buffer);
+                sb.Append(Encoding.UTF8.GetString(buffer));
+            } while (responseSocket.Available > 0);
+
+            Console.WriteLine(sb);
         }
     }
 }
